42888b502ad2ec1222d48eef20eb05ad3ff026ee
jasonhillinger <jasonhillinger@gmail.com>
2021-10-14 22:25:25 -0400

    addRegistration page added for SQL php integration of new registered accounts

---
+++ addRegistration.php
@@ -0,0 +1,39 @@
+<?php
+ include_once 'server.php';
+$errors = array();
+
+// Registering a new person to the database
+
+//$userID = mysqli_real_escape_string($db, $_POST['userID']); //not sure if this should be added or handled by db?
+$username = mysqli_real_escape_string($db, $_POST['username']);
+$email = mysqli_real_escape_string($db, $_POST['Email']);
+$password = mysqli_real_escape_string($db, $_POST['password']);
+
+//check db for existing person unique values
+$user_check_query = "SELECT * FROM users WHERE email = '$email' or username ='$username'"; // this will be an SQL querie (SELECT statement) to check if primary key already exist
+
+$results = mysqli_query($db, $user_check_query);
+$user = mysqli_fetch_assoc($results);
+
+if($user){
+
+  //PRIVATEKEY is a placeholder variable for a private key  
+  if($user['email'] === $email){array_push($errors, "Account already exist with this email!");}
+  if($user['username'] === $username){array_push($errors, "Account already exist with this username!");}
+
+}
+  if(count($errors) == 0){
+
+    $query = "INSERT INTO users (userID, username, password, email) VALUES ('$userID','$username','$password','$email'"; //This is where the SQL querie will go (Insert statement), to add the person if no errors
+    mysqli_query($db,$query);
+    
+    // redirect to submitted page
+    // may be changed depending on html implementation
+    header("Location: Sumbit.php");
+
+  }
+  else{
+      //Failed.php is a placeholder for an unsuccesful request
+      //May be changed depending on html implementation
+    header("Location: Failed.php");
+}
