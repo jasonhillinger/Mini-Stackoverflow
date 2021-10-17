3285e06bad7b7b908f80247fc2ede2000548b295
Jonathan Perlman <jonathan.perlman@mail.concordia.ca>
2021-10-16 23:59:03 -0400

    Update users.sql
    Update to users.sql to correct a bug in the SQL code.

--- users.sql
+++ users.sql
@@ -2,8 +2,8 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Oct 14, 2021
+-- Host: 192.168.1.170
+-- Generation Time: Oct 17, 2021 at 04:07 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -28,18 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
-  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` int(11) NOT NULL,
   `username` text NOT NULL,
   `password` text NOT NULL,
   `email` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`userID`, `username`, `password`, `email`) VALUES
-(58271, 'jsmith', 'APPLE12345', 'jsmith@example.ex');
 
 --
 -- Indexes for dumped tables
@@ -51,6 +44,16 @@ INSERT INTO `users` (`userID`, `username`, `password`, `email`) VALUES
 ALTER TABLE `users`
   ADD PRIMARY KEY (`userID`),
   ADD UNIQUE KEY `username` (`username`) USING HASH;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
