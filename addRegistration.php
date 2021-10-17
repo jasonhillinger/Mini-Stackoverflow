30579f7a3ed5c72cb1410f5bc7d52d634402a059
Jonathan Perlman <jonathan.perlman@mail.concordia.ca>
2021-10-16 23:57:32 -0400

    Update registration system
    Some fixes to registration system to have it work properly, including waiting for form to be submitted before sending data to database. Merged RegistrationPage.html into registration.php to eliminate redundancy. Also changed links in index page.

--- addRegistration.php
+++ addRegistration.php
@@ -3,34 +3,44 @@
 $errors = array();
 
 // Registering a new person to the database
-$username = mysqli_real_escape_string($db, $_POST['username']);
-$email = mysqli_real_escape_string($db, $_POST['Email']);
-$password = mysqli_real_escape_string($db, $_POST['password']);
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+	$username = mysqli_real_escape_string($db, $_POST['username']);
+	$email = mysqli_real_escape_string($db, $_POST['Email']);
+	$password = mysqli_real_escape_string($db, $_POST['password']);
 
-//check db for existing person unique values
-$user_check_query = "SELECT * FROM users WHERE email = '$email' or username ='$username'"; // this will be an SQL querie (SELECT statement) to check if primary key already exist
+	//check db for existing person unique values
+	$user_check_query = "SELECT * FROM users WHERE email = '$email' or username ='$username'"; // this will be an SQL querie (SELECT statement) to check if primary key already exist
 
-$results = mysqli_query($db, $user_check_query);
-$user = mysqli_fetch_assoc($results);
+	$results = mysqli_query($db, $user_check_query);
+	$user = mysqli_fetch_assoc($results);
 
-if($user){
+	if($user){
 
-  if($user['email'] === $email){array_push($errors, "Account already exist with this email!");}
-  if($user['username'] === $username){array_push($errors, "Account already exist with this username!");}
+	  if($user['email'] === $email){array_push($errors, "Account already exist with this email!<br>");}
+	  if($user['username'] === $username){array_push($errors, "Account already exist with this username!<br>");}
 
-}
-  if(count($errors) == 0){
+	}
+	  if(count($errors) == 0){
 
-    $query = "INSERT INTO users (username, password, email) VALUES ('$username','$password','$email')"; //This is where the SQL querie will go (Insert statement), to add the person if no errors
-    mysqli_query($db,$query);
-    
-    // redirect to submitted page
-    // may be changed depending on html implementation
-    header("Location: Sumbit.php");
+		$query = "INSERT INTO users (username, password, email) VALUES ('$username','$password','$email')"; //This is where the SQL querie will go (Insert statement), to add the person if no errors
+		mysqli_query($db,$query);
+		
 
-  }
-  else{
-      //Failed.php is a placeholder for an unsuccesful request
-      //May be changed depending on html implementation
-    header("Location: Failed.php");
+		echo("User account created succesfully.<br>");
+		sleep(3);
+			// redirect to home page
+			// may be changed depending on html implementation
+		header("Location: index.html");
+		
+
+	  }
+	  else{
+		  //Failed.php is a placeholder for an unsuccesful request
+		  //May be changed depending on html implementation
+		//header("Location: Failed.php");
+		while(count($errors) != 0){
+			echo(array_pop($errors));
+		}
+		echo("User account creation failed. Please relaod this page and try again.<br>");
+	}
 }
