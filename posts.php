9e4c8f3351b5dbe59abf011a00c5042973b2a205
jasonhillinger <jasonhillinger@gmail.com>
2021-10-19 19:15:39 -0400

    fixed syntax error

--- posts.php
+++ posts.php
@@ -25,16 +25,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 
 		echo("Question posted successfully");
-		//sleep(2);
-			// redirect to home page
-			// may be changed depending on html implementation
 		echo("<meta http-equiv=\"refresh\" content=\"5; url=index.php\" />");
 
 	}
 	  else{
-		  //Failed.php is a placeholder for an unsuccesful request
-		  //May be changed depending on html implementation
-		//header("Location: Failed.php");
 		while(count($errors) != 0){
 			echo(array_pop($errors));
 		}
@@ -42,6 +36,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 		echo("<meta http-equiv=\"refresh\" content=\"5; url=index.php\" />");
 
 	}
+}
 
 	
 	else if($isResponding){
@@ -53,16 +48,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 	
 	
 			echo("Answer posted successfully");
-			//sleep(2);
-				// redirect to home page
-				// may be changed depending on html implementation
 			echo("<meta http-equiv=\"refresh\" content=\"5; url=index.php\" />");
 	
 		}
 		  else{
-			  //Failed.php is a placeholder for an unsuccesful request
-			  //May be changed depending on html implementation
-			//header("Location: Failed.php");
 			while(count($errors) != 0){
 				echo(array_pop($errors));
 			}
@@ -73,18 +62,5 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 	}
 
-
-	//check db for existing person unique values
-	//$user_check_query = "SELECT * FROM users WHERE email = '$email' or username ='$username'"; // this will be an SQL querie (SELECT statement) to check if primary key already exist
-
-	// $results = mysqli_query($db, $user_check_query);
-	// $user = mysqli_fetch_assoc($results);
-
-	// if($user){
-
-	//   if($user['email'] === $email){array_push($errors, "Account already exists with this email!<br>");}
-	//   if($user['username'] === $username){array_push($errors, "Account already exists with this username!<br>");}
-
-	// }
-	
+}
 ?>
