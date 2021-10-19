f58508fdf3ac1c45b3b2c459a1085e3e5939ba9e
jasonhillinger <jasonhillinger@gmail.com>
2021-10-18 21:07:34 -0400

    fix #31

--- registration.php
+++ registration.php
@@ -12,16 +12,16 @@ include_once 'addRegistration.php';
 </p>
 		<p>
 			<label for="Email" class="floatLabel">Email</label>
-			<input id="Email" name="Email" type="text">
+			<input id="Email" name="Email" type="text" required>
 		</p>
 		<p>
 			<label for="password" class="floatLabel">Password</label>
-			<input id="password" name="password" type="password">
+			<input id="password" name="password" type="password" required>
 			<span>Enter a password longer than 8 characters</span>
 		</p>
 		<p>
 			<label for="confirm_password" class="floatLabel">Confirm Password</label>
-			<input id="confirm_password" name="confirm_password" type="password">
+			<input id="confirm_password" name="confirm_password" type="password" required>
 			<span>Your passwords do not match</span>
 		</p>
 		<p>
