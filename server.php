91bca9b300808bafa2e3fa5fc18c1fbc2e04e550
Jonathan Perlman <jonathan.perlman@mail.concordia.ca>
2021-10-18 21:43:39 -0400

    Update server.php
    Update to server.php file to include a remotely hosted database

--- server.php
+++ server.php
@@ -1,15 +1,19 @@
 <?php
 
-$dbServername = "localhost";
-$user = "root";
-$password = "";
+$dbServername = "homep4.ddns.net";
+$user = "access";
+$password = "SOEN341&test321";
 $dbName = "soen341";
 
-$db = mysqli_connect($dbServername,$user,$password,$dbName) or die("Connection failed to database.");
+$db = mysqli_connect($dbServername,$user,$password,$dbName,3306) or die("Connection failed to database.");
 
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
+
 }
 
+else {
+	//echo "Connect Successfully. <br>Host info: " . mysqli_get_host_info($db) . "<br>";
+}
 
 ?>
