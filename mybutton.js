62f5c1388efebc7f6f56f1d9134b6fecbb61cd8d
Antoine Gaubil <90474617+antoinegaubil@users.noreply.github.com>
2021-10-25 16:01:58 -0400

    Delete mybutton.js

--- mybutton.js
+++
@@ -1,7 +0,0 @@
-var button = document.getElementById("upvote"),
-    count = 0;
-button.onclick = function() {
-    setTimeout(disableFunction, 1);
-    count += 1;
-    button.innerHTML = "Upvote: " + count;
-};
