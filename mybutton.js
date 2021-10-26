77c5c707466f2b8af0f0471ed60821a48030e1f2
Antoine Gaubil <90474617+antoinegaubil@users.noreply.github.com>
2021-10-26 17:08:51 -0400

    Update mybutton.js

--- mybutton.js
+++ mybutton.js
@@ -1,7 +1,6 @@
 var button = document.getElementById("upvote"),
-var button = document.getElementById("myPopup"),
     count = 0;
-popup.onclick = function() {
+button.onclick = function() {
     count += 1;
     button.innerHTML = "Upvote: " + count;
 };
