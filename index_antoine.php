1ed0daf7194153697984301a1be9de23d496795c
Antoine Gaubil <90474617+antoinegaubil@users.noreply.github.com>
2021-10-25 22:58:01 -0400

    Update index_antoine.php

--- index_antoine.php
+++ index_antoine.php
@@ -137,6 +137,51 @@ session_start(); //start a new session if not already started
             background-color: rgb(15, 184, 23);
             color: white;
         }
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    
+
+        .popup {
+        position: relative;
+        display: inline-block;
+        cursor: pointer;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+}
+
+/* The actual popup */
+.popup .popuptext {
+  visibility: hidden;
+  width: 160px;
+  background-color: #555;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 8px 0;
+  position: absolute;
+  z-index: 1;
+  bottom: 125%;
+  left: 50%;
+  margin-left: -80px;
+}
+
+/* Popup arrow */
+.popup .popuptext::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+
+/* Toggle this class - hide and show the popup */
+.popup .show {
+  visibility: visible;
+}
     </style>
 </head>
 
@@ -165,7 +210,12 @@ session_start(); //start a new session if not already started
         <h2> A fast and efficient website to ask your Tech questions </h2>
         <img src="" />
     </div>
+	
+    <div class="popup" onclick="myFunction()">
+        <span class="popuptext" id="myPopup">Login / Register Options </span>
+    </div>
 
+	
     <div class="container text-center">
         <div class="row">
             <div class="col-sm-3 well">
