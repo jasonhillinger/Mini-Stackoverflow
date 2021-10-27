80f525cced9ae014213766ddc830f20d792479fb
Antoine Gaubil <90474617+antoinegaubil@users.noreply.github.com>
2021-10-27 16:42:49 -0400

    Update mybutton.js

--- mybutton.js
+++ mybutton.js
@@ -1,26 +1,26 @@
-var button = document.getElementById("upvote"),
-var loggedin = false;
+//var button = document.getElementById("upvote"),
+//var loggedin = false;
  
-count = 0;
+//count = 0;
 
 //if (loggedin)
-  {
+  //{
 function myFunction() 
    {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
    }
-  }
+ // }
   
   //else
-  {
+  //{
    
-  button.onclick = function()
-  {
-  count += 1;
-  button.innerHTML = "Upvote: " + count; 
-  }
-  };
+  //button.onclick = function()
+  //{
+  //count += 1;
+  //button.innerHTML = "Upvote: " + count; 
+  //}
+ // };
 
    
 
