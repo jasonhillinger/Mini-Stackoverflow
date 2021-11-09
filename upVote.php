7f3a4f3c34c378cd7fc02d6f9758d5ea07fea519
Jason Hillinger <j_hillin@poise.encs.concordia.ca>
2021-11-09 16:58:34 -0500

    upvote code for sql implemented

--- upVote.php
+++ upVote.php
@@ -4,16 +4,16 @@ $errors = array();
 
 //needs question ID, count
 $count = mysqli_real_escape_string($db, $_POST['count']);
-$questionID = mysqli_real_escape_string($db, $_POST['questionID']);
+$questionID = mysqli_real_escape_string($db, $_SESSION['questionID']);
 
 //might need error checking
 
-// assuming count can be +1 or -1
-//updates the old voteCount with a +/- 1
-$query1 = "SELECT voteCount FROM questions WHERE questionID = $questionID";
-$currentVoteCount = mysqli_query($db,$query1);
-$count = $currentVoteCount + $count;
-//
+// // assuming count can be +1 or -1
+// //updates the old voteCount with a +/- 1
+// $query1 = "SELECT voteCount FROM questions WHERE questionID = $questionID";
+// $currentVoteCount = mysqli_query($db,$query1);
+// $count = $currentVoteCount + $count;
+// //
 
 //updates the voteCount of the question being voted on to the count from JS
 //might need to use an incremental approach depending on JS implementation
